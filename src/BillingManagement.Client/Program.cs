09a58d0b45786c197addae6ceddedd4d49c1b643
mi.nie <pichai.n602@gmail.com>
2026-07-07 20:12:09 +0700

    #BMS-0046 Fix Docker client API access

--- src/BillingManagement.Client/Program.cs
+++ src/BillingManagement.Client/Program.cs
@@ -7,7 +7,12 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("http://localhost:5170/") });
+var clientBaseAddress = new Uri(builder.HostEnvironment.BaseAddress);
+var apiBaseAddress = clientBaseAddress.Port == 5080
+    ? new Uri("http://localhost:5081/")
+    : new Uri("http://localhost:5170/");
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = apiBaseAddress });
 builder.Services.AddScoped<OwnerCompanyProfileClient>();
 
 await builder.Build().RunAsync();
